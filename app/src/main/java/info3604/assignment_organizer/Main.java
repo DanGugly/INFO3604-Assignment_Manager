9e1592c78aa502cf268cbc0db75a47c765fd444f
DanGugly <you@example.com>
2019-02-07 14:55:32 -0400

    Fixed merger error, Added menu and 2 options.

--- app/src/main/java/info3604/assignment_organizer/Main.java
+++ app/src/main/java/info3604/assignment_organizer/Main.java
@@ -1,7 +1,11 @@
 package info3604.assignment_organizer;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class Main extends AppCompatActivity {
 
@@ -9,5 +13,25 @@ public class Main extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override   //Builds main_menu.xml from menu resourse in res
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override   //Getting which menu item is selected and creating toasts when they are
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.add_assignment:
+                Toast.makeText(this, "Add Assignment selected.", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.add_course:
+                Toast.makeText(this, "Add course selected.", Toast.LENGTH_SHORT).show();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
