163a7268b6daf5c2cb5cb5c04a81b3f9267103fe
DanGugly <you@example.com>
2019-02-24 15:51:40 -0400

    Added template for checkpoint model.

--- app/src/main/java/info3604/assignment_organizer/models/Checkpoint.java
+++ app/src/main/java/info3604/assignment_organizer/models/Checkpoint.java
@@ -5,6 +5,45 @@ import java.util.Date;
 
 public class Checkpoint implements Serializable {
 
-    private Date selected;
+    private String checkID;
 
+    private String assID;
+
+    private String title;
+
+    private Date expected;
+
+    private String notes;
+
+    public Checkpoint(String assID, String title, Date expected, String notes){
+        this.assID = assID;
+        this.title = title;
+        this.expected = expected;
+        this.notes = notes;
+    }
+
+    public String getCheckID() { return checkID; }
+
+    public String getAssID() { return assID; }
+
+    public String getTitle() { return title; }
+
+    public Date getExpected() { return expected; }
+
+    public String getNotes() { return notes; }
+
+    public void setCheckID(String checkID) { this.checkID = checkID; }
+
+    public void setAssID(String assID) { this.assID = assID; }
+
+    public void setTitle(String title) { this.title = title; }
+
+    public void setExpected(Date expected) { this.expected = expected; }
+
+    public void setNotes(String notes) { this.notes = notes; }
+
+    @Override
+    public boolean equals(Object obj) {
+        return this.getCheckID().equals(((Checkpoint)obj).getCheckID());
+    }
 }
