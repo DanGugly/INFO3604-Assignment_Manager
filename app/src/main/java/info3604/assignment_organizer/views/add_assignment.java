740fb223d2584a312c07d0ee4be8dc3828cbcb9b
DanGugly <you@example.com>
2019-02-24 22:37:51 -0400

    Added views for Assignment and Course, xml still needs to be built.

---
+++ app/src/main/java/info3604/assignment_organizer/views/add_assignment.java
@@ -0,0 +1,14 @@
+package info3604.assignment_organizer.views;
+
+import androidx.appcompat.app.AppCompatActivity;
+import info3604.assignment_organizer.R;
+
+import android.os.Bundle;
+
+public class add_assignment extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_assignment);
+    }
+}
