7e3fe8a62fdb9ad2e3ce9d8deceb2359168bdc37
lysg12345 <liamydgilbert@hotmail.com>
2019-02-26 15:30:23 -0400

    Changes to Add-Assignment

--- app/src/main/java/info3604/assignment_organizer/Main.java
+++ app/src/main/java/info3604/assignment_organizer/Main.java
@@ -1,6 +1,9 @@
 package info3604.assignment_organizer;
 
 import androidx.appcompat.app.AppCompatActivity;
+import info3604.assignment_organizer.views.add_assignment;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -25,8 +28,16 @@ public class Main extends AppCompatActivity {
     @Override   //Getting which menu item is selected and creating toasts when they are
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()){
+            case R.id.main_menu:
+                Toast.makeText(this, "Main Menu.", Toast.LENGTH_SHORT).show();
+                Intent main = new Intent(this, Main.class);
+                finish();
+                startActivity(main);
+                return true;
             case R.id.add_assignment:
                 Toast.makeText(this, "Add Assignment selected.", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(this, add_assignment.class);
+                startActivity(intent);
                 return true;
             case R.id.add_course:
                 Toast.makeText(this, "Add course selected.", Toast.LENGTH_SHORT).show();
--- app/src/main/java/info3604/assignment_organizer/views/add_assignment.java
+++ app/src/main/java/info3604/assignment_organizer/views/add_assignment.java
@@ -1,14 +1,111 @@
 package info3604.assignment_organizer.views;
 
 import androidx.appcompat.app.AppCompatActivity;
+import info3604.assignment_organizer.Main;
 import info3604.assignment_organizer.R;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class add_assignment extends AppCompatActivity{
+    public TextView course1, course2, course3, course4, course5, course6;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_assignment);
+
+        course1 = findViewById(R.id.course1);
+
+
+
+    }
+
+
+
+//    write items to file
+    private void writeToFile(String data, Context context) {
+        try {
+            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(context.openFileOutput("config.txt", Context.MODE_PRIVATE));
+            outputStreamWriter.write(data);
+            outputStreamWriter.close();
+        }
+        catch (Exception e) {
+            Toast.makeText(this, "File write failed: " + e.toString(), Toast.LENGTH_LONG).show();
+        }
+    }
+
+//    Read items from file
+    private String readFromFile(Context context) {
+
+        String ret = "";
+
+        try {
+            InputStream inputStream = context.openFileInput("config.txt");
+
+            if ( inputStream != null ) {
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                String receiveString = "";
+                StringBuilder stringBuilder = new StringBuilder();
+
+                while ( (receiveString = bufferedReader.readLine()) != null ) {
+                    stringBuilder.append(receiveString);
+                }
+
+                inputStream.close();
+                ret = stringBuilder.toString();
+            }
+        }
+        catch (FileNotFoundException e) {
+            Toast.makeText(this, "File not found: " + e.toString(), Toast.LENGTH_LONG).show();
+        } catch (IOException e) {
+            Toast.makeText(this, "Can not read file: " + e.toString(), Toast.LENGTH_LONG).show();
+        }
+
+        return ret;
+    }
+
+    @Override   //Builds main_menu.xml from menu resourse in res
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override   //Getting which menu item is selected and creating toasts when they are
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.main_menu:
+                Toast.makeText(this, "Main Menu.", Toast.LENGTH_SHORT).show();
+                Intent main = new Intent(this, Main.class);
+                startActivity(main);
+                finish();
+                return true;
+            case R.id.add_assignment:
+                Toast.makeText(this, "Add Assignment selected.", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(this, add_assignment.class);
+                finish();
+                startActivity(intent);
+                return true;
+            case R.id.add_course:
+                Toast.makeText(this, "Add course selected.", Toast.LENGTH_SHORT).show();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
