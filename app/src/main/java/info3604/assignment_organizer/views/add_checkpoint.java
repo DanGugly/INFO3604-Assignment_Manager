eea9df24b827d429e5faab32aaa7ab29260760ad
Elijah <elijahmckoy1997@hotmail.com>
2019-03-13 12:39:50 -0400

    Added functionality for inputting checkpoint data.

--- app/src/main/java/info3604/assignment_organizer/views/add_checkpoint.java
+++ app/src/main/java/info3604/assignment_organizer/views/add_checkpoint.java
@@ -3,13 +3,100 @@ package info3604.assignment_organizer.views;
 import androidx.appcompat.app.AppCompatActivity;
 import info3604.assignment_organizer.R;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TimePicker;
+import android.widget.Toast;
 
-public class add_checkpoint extends AppCompatActivity {
+import com.google.android.material.textfield.TextInputEditText;
+
+import java.util.Calendar;
+
+public class add_checkpoint extends AppCompatActivity implements DatePickerDialog.OnDateSetListener, TimePickerDialog.OnTimeSetListener {
+
+    Button save;
+    Button b_pick;
+    TextInputEditText tv_result; //Date and time text field
+    TextInputEditText checkName;
+    TextInputEditText checkTitle;
+    TextInputEditText checkNotes;
+
+    int day, month, year, hour, minute;
+    int dayFinal, monthFinal, yearFinal, hourFinal, minuteFinal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_checkpoint);
+
+        save = (Button) findViewById(R.id.save);
+        b_pick = (Button) findViewById(R.id.b_pick);
+
+        tv_result = (TextInputEditText) findViewById(R.id.tv_result);
+        checkName = (TextInputEditText) findViewById(R.id.checkName);
+        checkTitle = (TextInputEditText) findViewById(R.id.checkTitle);
+        checkNotes = (TextInputEditText) findViewById(R.id.checkNotes);
+
+        save.setOnClickListener(new View.OnClickListener() {  //What should happen when save button clicked?
+            @Override
+            public void onClick(View v) {                     //Maybe call controller method pass assignment info, let controller / model save to database
+                // For now I display info in toast
+                Toast.makeText(getApplicationContext(),
+                        "Name: "+checkName.getText()+
+                                " Title: " +checkTitle.getText()+
+                                " Date/Time: "+tv_result.getText()+
+                                " Notes: "+checkNotes.getText(),
+                        Toast.LENGTH_LONG).show();
+
+                //Can create intent here to go to view assignment page?
+            }
+        });
+
+        b_pick.setOnClickListener(new View.OnClickListener() {  //Button for setting date and time
+            @Override
+            public void onClick(View v) {
+                Calendar c = Calendar.getInstance();
+                year = c.get(Calendar.YEAR);
+                month = c.get(Calendar.MONTH);
+                day = c.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog datePickerDialog = new DatePickerDialog(add_checkpoint.this,
+                        add_checkpoint.this, year, month, day);
+                datePickerDialog.show();
+            }
+        });
     }
+
+    @Override
+    public void onDateSet(DatePicker datePicker, int i, int i1, int i2){
+        yearFinal = i;
+        monthFinal = i1 + 1;
+        dayFinal = i2;
+
+        Calendar c = Calendar.getInstance();
+        hour = c.get(Calendar.HOUR_OF_DAY);
+        minute = c.get(Calendar.MINUTE);
+
+        TimePickerDialog timePickerDialog = new TimePickerDialog(add_checkpoint.this,
+                add_checkpoint.this, hour, minute, true);
+        timePickerDialog.show();
+    }
+
+    @Override
+    public void onTimeSet(TimePicker timePicker, int i, int i1){ //This is date format returned when clicking set date & time button in Add Assignment
+        hourFinal = i;
+        minuteFinal = i1;
+
+        tv_result.setText(dayFinal+"/"+
+                monthFinal+"/"+
+                yearFinal+
+                " "+hourFinal+":"+
+                +minuteFinal
+        );
+    }
+
 }
